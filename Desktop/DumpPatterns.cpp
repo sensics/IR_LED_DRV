c51e5e36c2c6cddfdd545970aeddcf94b452228f
Ryan Pavlik <ryan@sensics.com>
2016-05-31 18:22:28 -0500

    A basic desktop app to unswizzle/extract the patterns for LEDs from firmware source.

---
+++ Desktop/DumpPatterns.cpp
@@ -0,0 +1,112 @@
+/** @file
+    @brief Implementation
+
+    @date 2016
+
+    @author
+    Sensics, Inc.
+    <http://sensics.com/osvr>
+*/
+
+// Copyright 2016 Sensics, Inc.
+//
+// All rights reserved.
+
+// Internal Includes
+extern "C" {
+#include "array_init.h"
+};
+
+// Library/third-party includes
+#include <json/value.h>
+#include <json/writer.h>
+
+// Standard includes
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+
+static const auto BITS = 8;
+static const auto NUM_LEDS = LED_LINE_LENGTH * BITS;
+
+static const auto TARGET0_BEACON_ORDER = {
+    5,  6,  3,  4,  1,  2,  10, 8,  9,  7,  11, 28, 27, 25, 15, 16, 17,
+    18, 19, 20, 21, 22, 23, 24, 14, 26, 13, 12, 29, 30, 31, 32, 33, 34};
+static const auto TARGET1_BEACON_ORDER = {37, 38, 39, 40, 33, 34};
+bool getElementBit(uint8_t element, uint8_t *arr) {
+  auto byte = element / BITS;
+  auto bit = element % BITS;
+  return 0x0 != ((arr[byte]) & (0x01 << bit));
+}
+
+bool getBitFromPattern(int patternElement, int led) {
+  return getElementBit(static_cast<uint8_t>(led),
+                       pattern_array[patternElement]);
+}
+
+template <typename ArrayType>
+std::string getPatternString(int element, ArrayType arr) {
+  std::string ret;
+  auto elementIdx = static_cast<uint8_t>(element);
+  for (int pattElt = 0; pattElt < PATTERN_COUNT; ++pattElt) {
+    ret += (getElementBit(elementIdx, arr[pattElt]) ? "*" : ".");
+  }
+  return ret;
+}
+
+int main() {
+  default_array_init();
+#if 0
+    std::vector<std::string> patterns(NUM_LEDS);
+    for (int patternElement = 0; patternElement < PATTERN_COUNT; ++patternElement) {
+        for (int led = 0; led < NUM_LEDS; ++led) {
+            patterns[led] += (getBitFromPattern(patternElement, led) ? "*" : ".");
+        }
+    }
+
+
+    for (int led = 0; led < NUM_LEDS; ++led) {
+        std::cout << "LED " << std::setw(2) << led << ": " << patterns[led]  << std::endl;
+    }
+#endif
+  for (int led = 0; led < NUM_LEDS; ++led) {
+    std::cout << "pattern_array LED " << std::setw(2) << led << ": "
+              << getPatternString(led, pattern_array) << std::endl;
+  }
+  std::cout << "\n\n";
+
+  {
+    Json::Value root;
+    auto addBeacon = [&](int beaconId) {
+      Json::Value beacon;
+      beacon[std::to_string(beaconId)] =
+          getPatternString(beaconId - 1, pattern_array);
+      root.append(beacon);
+    };
+    for (auto beaconId : TARGET0_BEACON_ORDER) {
+      // root[std::to_string(beaconId)] = getPatternString(beaconId - 1,
+      // pattern_array);
+      addBeacon(beaconId);
+    }
+    for (auto beaconId : TARGET1_BEACON_ORDER) {
+      // root[std::to_string(beaconId)] = getPatternString(beaconId - 1,
+      // pattern_array);
+
+      addBeacon(beaconId);
+    }
+    std::ofstream of("beacons.json");
+    if (of) {
+      of << root.toStyledString();
+      std::cout << "Wrote beacons.json file." << std::endl;
+    }
+  }
+
+  for (int elt = 0; elt < 2 * NUM_LEDS; ++elt) {
+    std::cout << "ir_led_driver_buffer " << std::setw(2) << elt << ": "
+              << getPatternString(elt, ir_led_driver_buffer) << std::endl;
+  }
+
+  return 0;
+}
