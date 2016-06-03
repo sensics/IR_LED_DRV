2b8930294420a8671390da1ebea78b54f695fc2f
Ryan Pavlik <ryan@sensics.com>
2016-06-03 13:16:08 -0500

    Add missing prototype in uart_protocol

--- User/uart_protocol.h
+++ User/uart_protocol.h
@@ -19,6 +19,7 @@
 
 #include "stm8s.h" // for uint8_t without conflicts
 
+void protocol_init();
 uint8_t protocol_is_output_ready();
 uint8_t protocol_get_output_byte();
 void protocol_put_input_byte( uint8_t ch );
