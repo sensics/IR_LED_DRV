0411241fa9a664b8dcbce8599624582787aef76d
Ryan Pavlik <ryan@sensics.com>
2016-07-13 17:31:54 -0500

    comment for program-ledhex

--- Program-LedHex.ps1
+++ Program-LedHex.ps1
@@ -5,6 +5,13 @@ Param(
     $Tool = "ST-LINK"
 )
 
+# Requires "ST Visual Programmer" installation, uses the default install location but can be overridden with -STVPDir argument.
+# Download from http://www.st.com/content/st_com/en/products/development-tools/software-development-tools/stm32-software-development-tools/stm32-programmers/stvp-stm32.html
+
+# Similarly, it requires a programming tool for the STM8: defaults to the ST-LINK family (including the ST-LINK v2 dongle), but that too can be overridden.
+# Pass the combined (flash and eeprom) hex file as the first or -HexFile argument. Ignore the warnings about address out of range: that's from passing the same hex
+# as both the flash file and the eeprom file.
+
 $STVPApp = Join-Path $STVPDir "STVP_CmdLine.exe"
 
 $STVPArgs = @(
