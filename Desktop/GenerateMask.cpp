a87d28b28c5d3c97770569a9437d7cbb5f2c1173
Ryan Pavlik <ryan@sensics.com>
2016-07-15 13:23:33 -0500

    Fix GenerateMask to use a custom (and correct) way of turning an array of bools into a bunch of hex bytes.

--- Desktop/GenerateMask.cpp
+++ Desktop/GenerateMask.cpp
@@ -21,7 +21,7 @@
 // - none
 
 // Standard includes
-#include <bitset>
+#include <cstddef>
 #include <iomanip>
 #include <iostream>
 #include <string>
@@ -34,20 +34,50 @@ static const auto DISABLED_TARGET0_BEACONS = {27};
 
 static BeaconOrderContainer TargetOrder0;
 
+static const int BITS_PER_BYTE = 8;
+
 int oneBasedTarget0BeaconToFirmwareBit(int beacon) { return TargetOrder0[beacon - 1]; }
+
+/// needs a bidirectional iterator
+template <typename Iterator> inline uint8_t byteFromBoolContainer(Iterator lsb) {
+  uint8_t ret = 0;
+  Iterator it = lsb;
+  // advancing one too far is OK because of the "one past the end" iterator.
+  std::advance(it, BITS_PER_BYTE);
+  for (int count = BITS_PER_BYTE; count != 0; --count) {
+    // can unconditionally move backwards because we moved it one too far.
+    --it;
+    if (*it) {
+      ret = ret << 1 | static_cast<uint8_t>(0x01);
+    } else {
+      ret = ret << 1;
+    }
+  }
+  return ret;
+}
+
+template <typename Container> inline uint8_t byteFromBoolContainerAtBit(Container &&c, std::size_t bit) {
+  auto it = begin(std::forward<Container>(c));
+  if (bit > 0) {
+    std::advance(it, bit);
+  }
+  return byteFromBoolContainer(it);
+}
+
+template <typename Container> inline uint8_t byteFromBoolContainerAtByte(Container &&c, std::size_t b) {
+  return byteFromBoolContainerAtBit(std::forward<Container>(c), b * BITS_PER_BYTE);
+}
 
 int main() {
   TargetOrder0.assign(begin(TARGET0_BEACON_ORDER), end(TARGET0_BEACON_ORDER));
-  std::bitset<NUM_LEDS> mask;
-  mask.set();
+  std::vector<bool> mask(NUM_LEDS, true);
   for (auto &beacon1based : DISABLED_TARGET0_BEACONS) {
     auto bit = oneBasedTarget0BeaconToFirmwareBit(beacon1based);
-    mask.reset(bit);
+    mask[bit] = false;
   }
 
-  uint64_t fullMask = mask.to_ullong();
   for (int i = 0; i < LED_LINE_LENGTH; ++i) {
-    uint8_t b = static_cast<uint8_t>((0xff << (i * 8) & fullMask) >> (i * 8));
+    auto b = byteFromBoolContainerAtByte(mask, i);
     std::cout << "0x" << std::hex << static_cast<unsigned>(b) << ", ";
   }
   std::cout << std::endl;
